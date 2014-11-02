15377d0c4d127c2b5c5c59dbf23edfc27b8c360d
Matthew Upson <ivyleavedtoadflax@gmail.com>
2014-11-02 13:14:45 +0000

    added hashing code

--- create_table.sql
+++ create_table.sql
@@ -18,59 +18,80 @@ birth_year int,
 gender int
 );
 
+-- Obviously you would need to adjust the file locations!
+-- Data available for download from http://www.citibikenyc.com/system-data
+
 .mode "csv"
-.import bike_data.csv nybikes
-.import '/home/matthew/Downloads/citibike/2013-07.csv' nybikes
-.import '/home/matthew/Downloads/citibike/2013-08.csv' nybikes
-.import '/home/matthew/Downloads/citibike/2013-09.csv' nybikes
-.import '/home/matthew/Downloads/citibike/2013-10.csv' nybikes
-.import '/home/matthew/Downloads/citibike/2013-11.csv' nybikes
-.import '/home/matthew/Downloads/citibike/2013-12.csv' nybikes
-.import '/home/matthew/Downloads/citibike/2014-01.csv' nybikes
-.import '/home/matthew/Downloads/citibike/2013-02.csv' nybikes
-.import '/home/matthew/Downloads/citibike/2013-03.csv' nybikes
-.import '/home/matthew/Downloads/citibike/2013-04.csv' nybikes
-.import '/home/matthew/Downloads/citibike/2013-05.csv' nybikes
-.import '/home/matthew/Downloads/citibike/2013-06.csv' nybikes
-.import '/home/matthew/Downloads/citibike/2013-07.csv' nybikes
-.import '/home/matthew/Downloads/citibike/2013-08.csv' nybikes
+.import '2013-07.csv' nybikes
+.import '2013-08.csv' nybikes
+.import '2013-09.csv' nybikes
+.import '2013-10.csv' nybikes
+.import '2013-11.csv' nybikes
+.import '2013-12.csv' nybikes
+.import '2014-01.csv' nybikes
+.import '2014-02.csv' nybikes
+.import '2014-03.csv' nybikes
+.import '2014-04.csv' nybikes
+.import '2014-05.csv' nybikes
+.import '2014-06.csv' nybikes
+.import '2014-07.csv' nybikes
+.import '2014-08.csv' nybikes
+
+-- each of these files has a header row, so lets get rid of all of those
 
 delete from nybikes where gender = 'gender';
 
+-- This is where I ran code in sqlitestudio
+-- you could just run all this code in sqlitestudio by uncommenting the lines below. This take a long time to complete however!
+
+--update nybikes set end_hash = substr(sha1(start_stn_id || start_stn_name || start_stn_lat || start_stn_long),1,6);
+--update nybikes set end_hash = substr(sha1(end_stn_id || end_stn_name || end_stn_lat || end_stn_long),1,6);
+
 drop table if exists stns;
 
+-- Note that id is non-unique, so can't be used as primary key
+
 create table stns(
-stn_id int primary key,
-stn_name text,
-stn_lat real,
-stn_long real
+hash text primary key,
+id int,
+name text,
+lat real,
+lon real
 );
 
-insert into stns select distinct start_stn_id,start_stn_name,start_stn_lat,start_stn_long from nybikes union select distinct end_stn_id,end_stn_name,end_stn_lat,end_stn_long from nybikes;
+-- Since there are journeys starting and stopping at all the stations, it's probably not necessary to look at both start and stop stations to get the complete list of stations... but let's do it anyway for completeness.
+
+insert into stns select distinct hash,start_stn_id,start_stn_name,start_stn_lat,start_stn_long from nybikes union select distinct end_hash,end_stn_id,end_stn_name,end_stn_lat,end_stn_long from nybikes;
+
+-- You cannot remove columns in sqlite, so we need to copy the table and create a new one without the columns we want to drop
 
 create temporary table nybikes1(
 tripduration int,
 starttime text,
 stoptime text,
-start_stn_id int,
-end_stn_id int,
+start_hash text,
+end_hash text,
 bikeid int,
 usertype text,
 birth_year int,
 gender int
 );
 
+-- Copy nybikes into temporary nybikes1
+
 insert into nybikes1 select tripduration,starttime,stoptime,
-start_stn_id,end_stn_id,bikeid,usertype,birth_year,gender from nybikes;
+start_hash,end_hash,bikeid,usertype,birth_year,gender from nybikes;
 
 drop table nybikes;
+
+-- now create a new smaller table, insert the rows as before, and drop the temporary table...done!
 
 create table nybikes(
 tripduration int,
 starttime text,
 stoptime text,
-start_stn_id int,
-end_stn_id int,
+start_hash int,
+end_hash int,
 bikeid int,
 usertype text,
 birth_year int,
@@ -81,4 +102,3 @@ insert into nybikes select tripduration,starttime,stoptime,
 start_stn_id,end_stn_id,bikeid,usertype,birth_year,gender from nybikes1;
 
 drop table nybikes1;
-
